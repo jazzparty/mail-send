abf14d51341fa3fdf419c933646b6cb962b01a3c
Jongyoun-mac17 <jazzparty72@gmail.com>
2017-10-02 12:31:03 +0900

    테스트.

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         send-mail
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project
+        Nuxt.js project Start
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
